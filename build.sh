4e69e91b054821f38566c2f960ca1b1036ac4033
Bai-Chiang <contact@bai-chiang.com>
2026-09-23 14:38:51 -0500

    Update build

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 script_dir="$( cd -- "$(dirname ${BASH_SOURCE[0]})" &> /dev/null ; pwd -P )"
 
 if [[ "${script_dir##*/}" == *cuda* && "${script_dir##*/}" != *cuda ]] ; then
-    args="$args --device /dev/nvidia0:/dev/nvidia0:r "
-    args="$args --device /dev/nvidiactl:/dev/nvidiactl:r "
-    args="$args --device /dev/nvidia-uvm:/dev/nvidia-uvm:r "
-    args="$args --device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools:r "
-    args="$args -v /usr/lib64/libnvidia-ml.so.1:/usr/lib64/libnvidia-ml.so.1:ro "
-    args="$args -v /usr/bin/nvidia-smi:/usr/bin/nvidia-smi:ro "
+    if [[ -f /usr/bin/sestatus ]] ; then
+        args="$args --security-opt=label=disable "
+    fi
+    if [[ -f /var/run/cdi/nvidia.yaml ]] ; then
+        args="$args --device nvidia.com/gpu=all "
+    else
+        args="$args --device /dev/nvidia0:/dev/nvidia0:r "
+        args="$args --device /dev/nvidiactl:/dev/nvidiactl:r "
+        args="$args --device /dev/nvidia-uvm:/dev/nvidia-uvm:r "
+        args="$args --device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools:r "
+        args="$args -v /usr/bin/nvidia-smi:/usr/bin/nvidia-smi:ro "
+    fi
 fi
 
 podman build \
     --tag "${script_dir##*/}" \
-    --build-arg username=tux \
+    --build-arg username=$(whoami) \
     ${args} \
     "${script_dir}"
--- run.sh
+++ run.sh
@@ -1,33 +1,39 @@
 script_dir="$( cd -- "$(dirname ${BASH_SOURCE[0]})" &> /dev/null ; pwd -P )"
 
-args="--workdir /home/tux/data "
-args="-v "${script_dir%/*}/data":/home/tux/data:z "
+args="-v "${script_dir%/*}/data":/workdir/data:z "
+if [[ "${script_dir##*/}" == archlinux* ]] ; then
+    shell=zsh
+else
+    shell=bash
+fi
 
 if [[ "${script_dir##*/}" == *cuda* ]] ; then
-    args="$args --device /dev/nvidia0:/dev/nvidia0:r "
-    args="$args --device /dev/nvidiactl:/dev/nvidiactl:r "
-    args="$args --device /dev/nvidia-uvm:/dev/nvidia-uvm:r "
-    args="$args --device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools:r "
-    args="$args -v /usr/lib64/libnvidia-ml.so.1:/usr/lib64/libnvidia-ml.so.1:ro "
-    args="$args -v /usr/bin/nvidia-smi:/usr/bin/nvidia-smi:ro "
+    if [[ -f /usr/bin/sestatus ]] ; then
+        args="$args --security-opt=label=disable "
+    fi
+    if [[ -f /var/run/cdi/nvidia.yaml ]] ; then
+        args="$args --device nvidia.com/gpu=all "
+    else
+        args="$args --device /dev/nvidia0:/dev/nvidia0:r "
+        args="$args --device /dev/nvidiactl:/dev/nvidiactl:r "
+        args="$args --device /dev/nvidia-uvm:/dev/nvidia-uvm:r "
+        args="$args --device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools:r "
+        args="$args -v /usr/bin/nvidia-smi:/usr/bin/nvidia-smi:ro "
+    fi
 fi
 
-if [[ "${script_dir##*/}" == *llamacpp* && "${script_dir##*/}" != *opencode ]] ; then
+if [[ "${script_dir##*/}" == *llamacpp* ]] ; then
     args="$args --publish 127.0.0.1:9931:9931 "
 fi
-
-if [[ "${script_dir##*/}" == *opencode ]] ; then
-    args="$args -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR"
-    args="$args -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY"
-    args="$args -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY"
-    args="$args --network none "
+if [[ "${script_dir##*/}" == *comfyui* ]] ; then
+    args="$args --publish 127.0.0.1:8188:8188 "
+    args="$args -v "${script_dir%/*}/data/comfyui":/workdir/comfyui/models:Z "
 fi
 
 podman run --rm -it --userns keep-id \
     --name "${script_dir##*/}" \
     --hostname "${script_dir##*/}" \
-    `#--security-opt=label=disable` \
     ${args} \
     "localhost/${script_dir##*/}" \
-    zsh -l
+    "${shell}" -l "${cmds[@]}"
 
